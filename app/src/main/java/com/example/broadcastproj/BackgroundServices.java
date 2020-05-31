9a6bf0d4b354525c17f3a96ae1875863816acb93
Rkeerthiga <keerthigaputtur21@gmail.com>
2020-05-31 13:31:06 +0530

    Broadcast Receivers

--- app/src/main/java/com/example/broadcastproj/BackgroundServices.java
+++ app/src/main/java/com/example/broadcastproj/BackgroundServices.java
@@ -1,18 +1,33 @@
 package com.example.broadcastproj;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
 
-import android.os.Bundle;
+public class BackgroundServices extends IntentService {
 
-public class BackgroundServices extends AppCompatActivity {
+    public BackgroundServices() {
+        super("BackgroundService");
+    }
 
-    public BackgroundServices(){
-        super(name:"BackgroundServices");
+    public static void startAction(Context context){
+        Intent intent = new Intent(context, BackgroundServices.class);
+        context.startService(intent);
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_background_services);
+    protected void onHandleIntent(Intent intent) {
+        if(intent != null){
+            for(int i = 0; i < 5; i++){
+                Intent localBroadcastIntent = new Intent(MainActivity.BROADCAST_ACTION);
+                localBroadcastIntent.putExtra("value", "Broadcast "+ (i + 1));
+                try{
+                    Thread.sleep(100);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                sendBroadcast(localBroadcastIntent);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/broadcastproj/MainActivity.java
+++ app/src/main/java/com/example/broadcastproj/MainActivity.java
@@ -2,19 +2,65 @@ package com.example.broadcastproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    public final static String BROADCAST_ACTION = "android.intent.action.MAIN";
+    Receiver localListener;
+    TextView txtViewMsg;
+    Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button=(Button)findViewById(R.id.btnbroadcast);
-        button.setOnClickListener(new View.onClickListener);
+        txtViewMsg = findViewById(R.id.txtBroadcast);
+        buttonStart = findViewById(R.id.btnBroadcast);
+//        buttonStart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                onClick();
+//            }
+//        });
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        localListener = new Receiver();
+        IntentFilter intentfilter =new IntentFilter(BROADCAST_ACTION);
+        this.registerReceiver(localListener, intentfilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.unregisterReceiver(localListener);
+    }
+
+    public  void onClick(View view){
+        if(view.getId() == R.id.btnBroadcast){
+            BackgroundServices.startAction(this.getApplicationContext());
+        }
+    }
+
+    public class Receiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String currentText = txtViewMsg.getText().toString();
+            String message = intent.getStringExtra("value");
+            currentText += "\nReceived "+ message;
+            txtViewMsg.setText(currentText);
+        }
+    }
 }
